998d025efa0fa9d6768a184ac96c18dd807c556c
shafayeatt1995 <shafayetalanik@gmail.com>
2025-09-29 16:26:44 +0600

    pwa added

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,3 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
@@ -7,5 +6,31 @@ export default defineNuxtConfig({
     public: { BASE_URL: process.env.BASE_URL, API_URL: process.env.API_URL },
   },
 
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@vite-pwa/nuxt"],
+  pwa: {
+    registerType: "autoUpdate",
+    strategies: "generateSW",
+    includeAssets: ["logo.png"],
+    manifest: {
+      name: "Bappy",
+      short_name: "Bappy",
+      description: "Bappy",
+      theme_color: "#ffffff",
+      background_color: "#ffffff",
+      display: "standalone",
+      start_url: "/",
+      icons: [
+        {
+          src: "/logo.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "/logo.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
+    },
+  },
 });

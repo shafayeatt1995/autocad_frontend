8b6af65425addf0bba57a2d383e0c91437b5b9f8
shafayeatt1995 <shafayetalanik@gmail.com>
2025-10-05 10:33:43 +0600

    google analytics added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Quicksand } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const quicksand = Quicksand({
   subsets: ["latin"],
@@ -14,6 +15,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="hydrated">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-M1BR7M2R2T"
+        ></Script>
+        <Script src="/js/gana.js"></Script>
+      </head>
       <body
         className={`${quicksand.className} antialiased`}
         data-new-gr-c-s-check-loaded="14.1256.0"
---
+++ public/js/gana.js
@@ -0,0 +1,6 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "G-M1BR7M2R2T");

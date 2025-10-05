e1021fe5be785bf5c5948b7465c8785978444c97
shafayeatt1995 <shafayetalanik@gmail.com>
2025-10-05 10:56:37 +0600

    gana added

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,6 +21,7 @@ export default function RootLayout({ children }) {
           src="https://www.googletagmanager.com/gtag/js?id=G-M1BR7M2R2T"
         ></Script>
         <Script src="/js/gana.js"></Script>
+        <Script src="/js/gtag.js"></Script>
       </head>
       <body
         className={`${quicksand.className} antialiased`}
@@ -28,6 +29,14 @@ export default function RootLayout({ children }) {
         data-gr-ext-installed=""
       >
         {children}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-ND7W9QSN"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
       </body>
     </html>
   );

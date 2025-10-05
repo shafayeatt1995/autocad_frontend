801e5350a070cfd3b440620d42e24f82de897da5
shafayeatt1995 <shafayetalanik@gmail.com>
2025-10-05 11:01:17 +0600

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -34,7 +34,7 @@ export default function RootLayout({ children }) {
             src="https://www.googletagmanager.com/ns.html?id=GTM-ND7W9QSN"
             height="0"
             width="0"
-            style="display:none;visibility:hidden"
+            className="hidden"
           ></iframe>
         </noscript>
       </body>

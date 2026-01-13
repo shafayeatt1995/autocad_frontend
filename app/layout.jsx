8267e44f580a951ef55b7f35cd8271260cf5efe8
Shafayet Al-Anik <shafayetalanik@gmail.com>
2026-01-13 20:19:27 +0600

    up

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,14 +15,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="hydrated">
-      {/* <head>
-        <Script
+      <head>
+        {/* <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-M1BR7M2R2T"
-        ></Script>
+        ></Script> */}
         <Script src="/js/gana.js"></Script>
-        <Script src="/js/gtag.js"></Script>
-      </head> */}
+        {/* <Script src="/js/gtag.js"></Script> */}
+      </head>
       <body className={`${font.className} antialiased`}>
         {children}
         <Toaster />
